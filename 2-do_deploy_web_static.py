324fb0cbb607078fc1974722628db1efc2792afd
weaver010 <abdelrahmannagaty839@gmail.com>
2024-03-07 13:50:39 -0800

    hi

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+ script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, 
+"""
+
+from os.path import exists
+from fabric.api import put, run, env
+
+env.hosts = ['54.87.160.77', '72.44.33.131']
+
+
+def do_deploy(archive_path):
+    """
+     archive file from local to my webservers
+    """
+
+    if not exists(archive_path):
+        return False
+    try:
+        file_name = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, "/tmp/")
+
+        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name))
+
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
+
+        run(('mv /data/web_static/releases/{}/web_static/* ' +
+            '/data/web_static/releases/{}/')
+            .format(file_name, file_name))
+
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
+
+        run('rm -rf /data/web_static/current')
+
+        run(('ln -s /data/web_static/releases/{}/' +
+            ' /data/web_static/current')
+            .format(file_name))
+        return True
+    except Exception:
+        return False
